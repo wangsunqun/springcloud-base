4c426d5e4a648d7dd032c766593b5d67da852784
wangsunqun <1805696978@qq.com>
2018-06-27 18:20:15 +0800

    [*]add provider

--- provider/provider-api/src/main/java/com/wsq/providerapi/feignInterface/UserInterface.java
+++ provider/provider-api/src/main/java/com/wsq/providerapi/feignInterface/UserInterface.java
@@ -2,13 +2,13 @@ package com.wsq.providerapi.feignInterface;
 
 import com.wsq.providerapi.dto.UserDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("provider")
 @RequestMapping("user")
 public interface UserInterface {
 
     @RequestMapping("getUserById")
-    UserDto getUserById(@PathVariable("id")long id);
+    UserDto getUserById(@RequestParam("id")long id);
 }
--- provider/provider-core/src/main/java/com/wsq/providercore/controller/UserController.java
+++ provider/provider-core/src/main/java/com/wsq/providercore/controller/UserController.java
@@ -2,15 +2,17 @@ package com.wsq.providercore.controller;
 
 import com.wsq.providerapi.dto.UserDto;
 import com.wsq.providerapi.feignInterface.UserInterface;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController implements UserInterface{
     @Override
-    public UserDto getUserById(long id) {
+    public UserDto getUserById(@RequestParam("id")long id) {
         UserDto dto = new UserDto();
         dto.setId(id);
         dto.setUserName("wang");
+        System.out.println("11111111111111111111");
         return dto;
     }
 }
