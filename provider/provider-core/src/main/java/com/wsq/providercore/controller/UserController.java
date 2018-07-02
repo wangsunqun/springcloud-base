d5f99aaee41b81a3ca0da8c0ae8f7e8da49abb9d
wangsunqun <fison.wang@nakedhub.com>
2018-07-02 23:08:09 +0800

    [*]优化

--- provider/provider-core/src/main/java/com/wsq/providercore/controller/UserController.java
+++ provider/provider-core/src/main/java/com/wsq/providercore/controller/UserController.java
@@ -2,16 +2,20 @@ package com.wsq.providercore.controller;
 
 import com.wsq.providerapi.dto.UserDto;
 import com.wsq.providerapi.feignInterface.UserInterface;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController implements UserInterface{
+    @Value("${server.port}")
+    private String port;
+
     @Override
     public UserDto getUserById(@RequestParam("id")long id) {
         UserDto dto = new UserDto();
         dto.setId(id);
-        dto.setUserName("wang");
+        dto.setUserName(port);
         System.out.println("11111111111111111111");
         return dto;
     }
