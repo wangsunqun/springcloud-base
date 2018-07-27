f319b0f79a4aade757ddc0c24ed0d6636bc8a2de
wangsunqun <1805696978@qq.com>
2018-07-27 17:36:48 +0800

    [*]add zuul & common

--- consumer/src/main/java/com/wsq/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/wsq/consumer/ConsumerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.wsq")
-@EnableDiscoveryClient
-@ServletComponentScan
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.wsq")
+@ServletComponentScan
+@EnableDiscoveryClient
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
--- zuul/src/main/java/com/wsq/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/wsq/zuul/ZuulApplication.java
@@ -2,11 +2,11 @@ package com.wsq.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
-@ServletComponentScan
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
 
